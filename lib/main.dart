234dcdb783aa8d235b1a08b582f2746db77f5ec0
Ricardo <recaldericardo73@hotmail.com>
2024-11-01 00:34:51 -0300

    Se agrego el agrupador por fecha y tambien se cambio el idioma del calendario al espanhol

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'reservas_screen.dart';
 
 void main() {
@@ -11,6 +12,14 @@ class AlpinaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('es', 'ES'), // Configuración en español
+      ],
       title: 'Gestión de Reservas',
       theme: ThemeData(
         primarySwatch: Colors.green,
