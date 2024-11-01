21bb31c65715c5af3c404b21b678f9f385d6e454
Ricardo <recaldericardo73@hotmail.com>
2024-11-01 14:28:47 -0300

    Se agregaron los parametros para integrar firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'reservas_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const AlpinaApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(AlpinaApp());
 }
 
 class AlpinaApp extends StatelessWidget {
   const AlpinaApp({super.key});
-
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       localizationsDelegates: const [
